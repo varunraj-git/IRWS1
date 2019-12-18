7ae234646085caaf3de9e4a70601bd39c10cd91b
Arun <arunraj.nexus5@gmail.com>
2019-12-18 00:13:29 -0500

    average precision calculation

--- src/com/griffith/sem1/irws/IRWS.java
+++ src/com/griffith/sem1/irws/IRWS.java
@@ -7,7 +7,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.print.attribute.HashAttributeSet;
 
 public class IRWS {
 
@@ -36,7 +40,13 @@ public class IRWS {
 
     private static void processInputData(final List<String> data) throws IOException {
 
+    	
         final BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+        /* key = engine# value = count */
+        final Map<String, Integer> searchEngineCount = new HashMap<>(); 
+        /* key = engine# value = precision count */
+        final Map<String, Double> searchEnginePrec = new HashMap<>();
+        
         for (final String inp : data) {
             final String arr[] = inp.split(";");
             final int rel = Integer.parseInt(arr[3]);
@@ -45,14 +55,30 @@ public class IRWS {
             final double precision = (double) relret / (double) ret;
             final double recall = (double) relret / (double) rel;
             final double patFive = getPatNumber(arr[2], 5);
-            final double patn = getPatNumber(arr[2], rel);
+            double patr05 = 0.0;
+            if(recall >=  0.5 && recall <0.6) {
+            	patr05 = precision;
+            }
             final double averagePrecision = getAveragePrecision(arr[2], rel);
+            if(searchEngineCount.get(arr[1]) !=null ) {
+            	searchEngineCount.put(arr[1], searchEngineCount.get(arr[1])+1);
+            	searchEnginePrec.put(arr[1], searchEnginePrec.get(arr[1]) + precision);
+            }else {
+            	searchEngineCount.put(arr[1], 1);
+            	searchEnginePrec.put(arr[1], precision);
+            }
 
-            writer.write("Precision: " + precision + ", Recall: " + recall + ", P@5: " + patFive +
-                    ", p@n: " + patn + ", averageP: " + averagePrecision);
+            writer.write(arr[1] +" - Precision: " + precision + ", Recall: " + recall + ", P@5: " + patFive +
+                    ", patr=0.5: " + patr05+", averageP: " + averagePrecision);
             writer.newLine();
         }
         writer.close();
+        
+        System.out.println(searchEnginePrec);
+        searchEngineCount.entrySet().forEach(entry -> {
+        	double averagePrecision = searchEnginePrec.get(entry.getKey())/entry.getValue();
+        	System.out.println("Average Prec: " + entry.getKey() + " => " + averagePrecision);
+        });
 
     }
 
